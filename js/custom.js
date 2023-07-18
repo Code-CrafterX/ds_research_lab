122944fee5a58c587b035bd598989db2bad66f26
Abhinav_SB <abhinavsb25@gmail.com>
2023-07-19 00:45:31 +0530

    More info funcitonality added

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,31 @@
-(function($) {
-    "use strict";
-	
+(function ($) {
+	"use strict";
+
 	/* ..............................................
 	Loader 
-    ................................................. */
-	
-	$(window).on('load', function() { 
-		$('.preloader').fadeOut(); 
-		$('#preloader').delay(550).fadeOut('slow'); 
-		$('body').delay(450).css({'overflow':'visible'});
+	................................................. */
+
+	$(window).on('load', function () {
+		$('.preloader').fadeOut();
+		$('#preloader').delay(550).fadeOut('slow');
+		$('body').delay(450).css({ 'overflow': 'visible' });
 	});
-    	
+
 	/* ..............................................
-    Navbar Bar
-    ................................................. */
-	
-	$('.navbar-nav .nav-link').on('click', function() {
+	Navbar Bar
+	................................................. */
+
+	$('.navbar-nav .nav-link').on('click', function () {
 		var toggle = $('.navbar-toggler').is(':visible');
 		if (toggle) {
 			$('.navbar-collapse').collapse('hide');
 		}
 	});
-	
+
 	/* ..............................................
-    Fixed Menu
-    ................................................. */
-    
+	Fixed Menu
+	................................................. */
+
 	$(window).on('scroll', function () {
 		if ($(window).scrollTop() > 50) {
 			$('.top-header').addClass('fixed-menu');
@@ -35,8 +35,8 @@
 	});
 
 	/* ..............................................
-    Properties Filter
-    ................................................. */
+	Properties Filter
+	................................................. */
 	var Container = $('.container');
 	Container.imagesLoaded(function () {
 		var portfolio = $('.properties-menu');
@@ -54,10 +54,10 @@
 	});
 
 	/* ..............................................
-    Gallery
-    ................................................. */
-	
-	$(document).ready(function() {
+	Gallery
+	................................................. */
+
+	$(document).ready(function () {
 		$('.popup-gallery').magnificPopup({
 			delegate: 'a',
 			type: 'image',
@@ -66,21 +66,21 @@
 			gallery: {
 				enabled: true,
 				navigateByImgClick: true,
-				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+				preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
 			},
 			image: {
 				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-				titleSrc: function(item) {
+				titleSrc: function (item) {
 					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
 				}
 			}
 		});
 	});
-	
+
 	/* ..............................................
-    Scroll To Top
-    ................................................. */
-	
+	Scroll To Top
+	................................................. */
+
 	$(document).ready(function () {
 
 		$(window).scroll(function () {
@@ -99,9 +99,15 @@
 		});
 
 	});
-	
-	
-
-	
 }(jQuery));
+
+// Show more button in index.html
+const showMoreBtn = document.getElementById('more-info-button');
+const content = document.getElementById('show-more-content');
+
+showMoreBtn.addEventListener('click', () => {
+	content.classList.toggle('visible');
+});
+
+
 
